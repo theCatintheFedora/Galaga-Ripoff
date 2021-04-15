c2affa460c8a2b4e991a2043b236aac649439b88
Timothy Price <theCatintheFedora1367@gmail.com>
2021-04-15 10:32:07 -0500

    fixed background

--- Galaga Ripoff/GameScene.swift
+++ Galaga Ripoff/GameScene.swift
@@ -10,7 +10,7 @@ import GameplayKit
 
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
-        // this stuff happens once (when the app opens)
+        createBackground()
     }
     func createBackground() {
         let stars = SKTexture(imageNamed: "Stars")

84b6f5ff134cdc5650b44b25b25bf2309a5e6ec7
Timothy Price <theCatintheFedora1367@gmail.com>
2021-04-20 10:23:41 -0500

    worked on the enemy fighter

--- Galaga Ripoff/GameScene.swift
+++ Galaga Ripoff/GameScene.swift
@@ -9,8 +9,12 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    
+    var enemy = SKSpriteNode()
     override func didMove(to view: SKView) {
         createBackground()
+        
+        makeEnemy()
     }
     func createBackground() {
         let stars = SKTexture(imageNamed: "Stars")
@@ -26,4 +30,21 @@ class GameScene: SKScene {
             starsBackground.run(moveForever)
         }
     }
+    
+    func makeEnemy() {
+        enemy.removeFromParent()
+        enemy = SKSpriteNode(imageNamed:"Enemy fighter")
+        enemy.position = CGPoint(x: frame.midX, y: frame.maxY - 50)
+        enemy.name = "Enemy"
+        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+        enemy.physicsBody?.isDynamic = false
+        enemy.physicsBody?.usesPreciseCollisionDetection = true
+        enemy.physicsBody?.friction = 0
+        enemy.physicsBody?.affectedByGravity = false
+        enemy.physicsBody?.restitution = 1
+        enemy.physicsBody?.linearDamping = 0
+        enemy.physicsBody?.contactTestBitMask = (enemy.physicsBody?.collisionBitMask)!
+        addChild(enemy)
+    }
 }
+
